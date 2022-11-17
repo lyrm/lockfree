5a80dd0132af2b9eb5ce9e96e49912ec02702b0e
Carine Morel <carine@tarides.com>
2022-11-17 19:29:34 +0100

    First try (needs debugging)

--- src/spmc_queue.ml
+++ src/spmc_queue.ml
@@ -13,25 +13,53 @@ type 'a t = {
   head : int Atomic.t;
   mask : int;
   size_exponent : int;
-  array : 'a option Atomic.t Array.t;
+  array : 'a node option Atomic.t Array.t;
   tail : int Atomic.t;
+  help_ind : int Atomic.t;
+  help_arr : 'a help Atomic.t Array.t;
+  help_mask : int;
 }
+
+and 'a node = { ind : int option; value : 'a }
+and 'a help = { free : bool; v : 'a option }
 
 let create ~size_exponent () =
   let size = Int.shift_left 1 size_exponent in
+  let help_size = 128 in
   {
     head = Atomic.make 0;
     mask = size - 1;
     tail = Atomic.make 0;
     size_exponent;
     array = Array.init size (fun _ -> Atomic.make None);
+    help_ind = Atomic.make 0;
+    help_arr =
+      Array.init help_size (fun _ -> Atomic.make { free = true; v = None });
+    (* size should be >= max number of domains (or more precisely, stealers) *)
+    help_mask = help_size - 1;
   }
 
 let indicative_size { head; tail; _ } =
   max (Atomic.get tail - Atomic.get head) 0
 
 module Local = struct
-  let push { tail; head; mask; array; _ } element =
+  let rec book_helping_cell ({ help_ind; help_arr; help_mask; _ } as t) : int =
+    let help_ind_val = Atomic.fetch_and_add help_ind 1 in
+    let help_cell = help_arr.(help_ind_val land help_mask) in
+    let help_cell_val = Atomic.get help_cell in
+    match help_cell_val with
+    | { free = true; v = None } ->
+        if
+          Atomic.compare_and_set help_cell help_cell_val
+            { free = false; v = None }
+        then help_ind_val
+        else book_helping_cell t
+    | { free = false; _ } ->
+        (* not free ! As each domain can only have one cell, it should not be a blocking issue *)
+        book_helping_cell t
+    | { free = true; _ } -> assert false
+
+  let rec push ({ tail; head; mask; array; _ } as t) element =
     let tail_val = Atomic.get tail in
     let head_val = Atomic.get head in
     let size = mask + 1 in
@@ -41,13 +69,139 @@ module Local = struct
     else
       let index = tail_val land mask in
       let cell = Array.get array index in
-      while Option.is_some (Atomic.get cell) do
-        ()
-      done;
-      Atomic.set cell (Some element);
+      (match Atomic.get cell with
+      | None -> ()
+      | Some _ -> help_steal t tail_val);
+      Atomic.set cell (Some { value = element; ind = None });
       Atomic.set tail (tail_val + 1);
-      true;;
-  
+      true
+
+  and help_steal { mask; array; help_arr; _ } tail_val : unit =
+    let cell_index = tail_val land mask in
+    let cell = Array.get array cell_index in
+    match Atomic.get cell with
+    | None -> () (* stealer has finished stealing *)
+    | Some { value; ind } as node -> (
+        match ind with
+        | None -> () (* stealer has finished stealing *)
+        | Some hind ->
+            if
+              Atomic.compare_and_set help_arr.(hind) { free = false; v = None }
+                { free = false; v = Some value }
+            then
+              (* value to be stolen is stocked in the help array,
+                   we can try to free the cell *)
+              ignore (Atomic.compare_and_set cell node None)
+              (* either it works and the stealer can retrieve the value
+                 in the help array or this CAS did not work, which means
+                 the stealer has done its jobs -> both case are Ok. *)
+            else () (* stealer has finished stealing *))
+
+  and steal_one ({ head; tail; mask; array; help_arr; help_mask; _ } as t) =
+    (* assumes there's space in the queue *)
+    let tail_val = Atomic.get tail in
+    let head_val = Atomic.get head in
+    let size = tail_val - head_val in
+    if size < 1 then raise Exit
+    else
+      let help_ind = book_helping_cell t land help_mask in
+      let new_head_val = head_val + 1 in
+      let acquired_item = Atomic.compare_and_set head head_val new_head_val in
+      if not acquired_item then (
+        assert (
+          Atomic.compare_and_set help_arr.(help_ind) { free = false; v = None }
+            { free = true; v = None });
+        raise Exit)
+      else
+        let index = head_val land mask in
+        let cell = Array.get array index in
+        let node = Atomic.get cell in
+        if Atomic.compare_and_set cell node None then (
+          (* free the help cell *)
+          Atomic.set help_arr.(help_ind) { free = true; v = None };
+          let { value; _ } = Option.get node in
+          value)
+        else
+          match Atomic.get help_arr.(help_ind) with
+          | { free = false; v } ->
+              assert (Option.is_some v);
+              (* free the help cell *)
+              Atomic.set help_arr.(help_ind) { free = true; v = None };
+              Option.get v
+          | _ -> assert false
+
+  and steal ~from ({ mask = local_mask; _ } as local) =
+    (* need to be careful with from queue, which is not local *)
+    let local_size = local_mask + 1 in
+    let ({
+           head = from_head;
+           tail = from_tail;
+           mask = from_mask;
+           array = from_array;
+           help_mask = from_help_mask;
+           help_arr = from_help_arr;
+           _;
+         }
+          : 'a t) =
+      from
+    in
+    (* assumes there's space in the queue *)
+    let from_tail_val = Atomic.get from_tail in
+    let from_head_val = ref (Atomic.get from_head) in
+    let from_size = from_tail_val - !from_head_val in
+    if from_size < 1 then 0
+    else
+      let from_help_ind = book_helping_cell from land from_help_mask in
+      let max_to_steal =
+        (* steal even if there's a single element, thus *+1* *)
+        min ((from_size + 1) / 2) local_size
+      in
+      let stolen = ref 0 in
+      let break = ref false in
+
+      while (not !break) && !stolen < max_to_steal do
+        let new_head_val = !from_head_val + 1 in
+        let acquired_item =
+          Atomic.compare_and_set from_head !from_head_val new_head_val
+        in
+
+        if not acquired_item then (
+
+          if not (Atomic.compare_and_set
+                    from_help_arr.(from_help_ind)
+                    { free = false; v = None } { free = true; v = None }) then
+          failwith "steal issue #1";
+          break := true)
+        else (
+          if not (from_tail_val - !from_head_val >= 0) then failwith "steal issue #2";
+          let from_index = !from_head_val land from_mask in
+          let cell = Array.get from_array from_index in
+          let node = Atomic.get cell in
+          let value = (Option.get node).value in
+          let to_push =
+            if Atomic.compare_and_set cell node None then (
+              (* Steal succeeds. We can now free the help cell *)
+              Atomic.set from_help_arr.(from_help_ind) { free = true; v = None };
+              value)
+            else
+              (* Producer has finished the steal *)
+              match Atomic.get from_help_arr.(from_help_ind) with
+              | { free = false; v } ->
+                  (* free the help cell *)
+                  Atomic.set
+                    from_help_arr.(from_help_ind)
+                    { free = true; v = None };
+                  Option.get v
+              | _ -> failwith "steal issue #3"
+          in
+          incr stolen;
+          let pushed = push local to_push in
+          (* What should we do if `push` has failed ? *)
+          if not pushed then failwith "steal issue #4";
+          incr from_head_val)
+      done;
+      !stolen
+
   let pop { head; tail; mask; array; _ } : 'a option =
     (* dequeue is optimistic because it can use the fact that
        there is no enqueuer *)
@@ -64,13 +218,10 @@ module Local = struct
     else
       let index = old_head_val land mask in
       let cell = Array.get array index in
-      let item = ref (Atomic.get cell) in
-      while
-        not (Option.is_some !item && Atomic.compare_and_set cell !item None)
-      do
-        item := Atomic.get cell
-      done;
-      !item
+      let item = Atomic.get cell in
+      assert (Option.is_some item);
+      assert (Atomic.compare_and_set cell item None);
+      Some (Option.get item).value
 
   (* successfuly rolled back *)
   let is_empty_thorough { head = _; tail; mask; array; _ } : bool =
@@ -90,108 +241,37 @@ module Local = struct
     let tail_val = Atomic.get tail in
     let head_val = Atomic.get head in
     if tail_val - head_val != 0 then false else is_empty_thorough queue
-
-  let steal ~from ({ mask = local_mask; _ } as local) =
-    (* need to be careful with from queue, which is not local *)
-    let local_size = local_mask + 1 in
-    let ({
-           head = from_head;
-           tail = from_tail;
-           mask = from_mask;
-           array = from_array;
-           _;
-         }
-          : 'a t) =
-      from
-    in
-    (* assumes there's space in the queue *)
-    let from_tail_val = Atomic.get from_tail in
-    let from_head_val = Atomic.get from_head in
-    let from_size = from_tail_val - from_head_val in
-    if from_size < 1 then 0
-    else
-      let stealable =
-        (* steal even if there's a single element, thus *+1* *)
-        min ((from_size + 1) / 2) local_size
-      in
-      let new_from_head_val = from_head_val + stealable in
-      let acquired_items =
-        Atomic.compare_and_set from_head from_head_val new_from_head_val
-      in
-      assert (from_tail_val - new_from_head_val >= 0);
-      if not acquired_items then 0
-      else (
-        for i = 0 to stealable - 1 do
-          let from_index = (from_head_val + i) land from_mask in
-          let cell = Array.get from_array from_index in
-          let value = Atomic.get cell in
-          assert (Atomic.compare_and_set cell value None);
-          assert (Option.is_some value);
-          let value_exn = function None -> assert false | Some v -> v in
-          while not (push local (value_exn value)) do
-            ()
-          done
-        done;
-        stealable)
-
-  let steal_one { head; tail; mask; array; _ } =
-    (* assumes there's space in the queue *)
-    let tail_val = Atomic.get tail in
-    let head_val = Atomic.get head in
-    let size = tail_val - head_val in
-    if size < 1 then raise Exit
-    else
-      let new_head_val = head_val + 1 in
-      let acquired_item = Atomic.compare_and_set head head_val new_head_val in
-      if not acquired_item then raise Exit
-      else
-        let index = head_val land mask in
-        let cell = Array.get array index in
-        let value = Atomic.get cell in
-        assert (Atomic.compare_and_set cell value None);
-        assert (Option.is_some value);
-        match value with None -> assert false | Some v -> v
 end
 
-
-
-module Resizable = struct 
+module Resizable = struct
   type nonrec 'a t = 'a t Atomic.t
-  
+
   module Local = struct
-    let resize (t : 'a t) : unit = 
-      let t_val = Atomic.get t in 
-      let ({size_exponent; _}) = Atomic.get t in 
-      let new_t_val = create ~size_exponent:(size_exponent+1) () in 
+    let resize (t : 'a t) : unit =
+      let t_val = Atomic.get t in
+      let { size_exponent; _ } = Atomic.get t in
+      let new_t_val = create ~size_exponent:(size_exponent + 1) () in
       (* transfer all elements *)
-      let finished = ref false in 
-      while not !finished do 
-        match Local.pop t_val with 
-        | None -> finished := true 
-        | Some item -> 
-          assert (Local.push new_t_val item);
-      done; 
+      let finished = ref false in
+      while not !finished do
+        match Local.pop t_val with
+        | None -> finished := true
+        | Some item -> assert (Local.push new_t_val item)
+      done;
       (* save the new queue *)
       Atomic.set t new_t_val
-    ;;
 
-      
     let rec push_with_autoresize (t : 'a t) (element : 'a) =
-      let t_val = Atomic.get t in 
-      if Local.push t_val element 
-      then () 
-      else 
-        (resize t;
-        push_with_autoresize t element);;
-    
-    let push t item = Local.push (Atomic.get t) item 
-    
-    let pop t = Local.pop (Atomic.get t) 
+      let t_val = Atomic.get t in
+      if Local.push t_val element then ()
+      else (
+        resize t;
+        push_with_autoresize t element)
 
+    let push t item = Local.push (Atomic.get t) item
+    let pop t = Local.pop (Atomic.get t)
     let steal ~from t = Local.steal ~from:(Atomic.get from) (Atomic.get t)
-      
     let steal_one t = Local.steal_one (Atomic.get t)
-
     let is_empty t = Local.is_empty (Atomic.get t)
   end
 
@@ -205,9 +285,10 @@ module M = struct
   *)
 
   let create () = Resizable.create ~size_exponent:5 ()
-  let push : ('a Resizable.t -> 'a -> unit) = Resizable.Local.push_with_autoresize
+  let push : 'a Resizable.t -> 'a -> unit = Resizable.Local.push_with_autoresize
 
-  let pop t = match Resizable.Local.pop t with Some v -> v | None -> raise Exit
+  let pop t =
+    match Resizable.Local.pop t with Some v -> v | None -> raise Exit
 
   let steal = Resizable.Local.steal_one
 end
--- src/spmc_queue.mli
+++ src/spmc_queue.mli
@@ -1,20 +1,14 @@
 (* add lifo slot? *)
 
-type 'a t = {
-  head : int Atomic.t;
-  mask : int;
-  size_exponent : int;
-  array : 'a option Atomic.t Array.t;
-  tail : int Atomic.t;
-}
+type 'a t
 
 val create : size_exponent:int -> unit -> 'a t
 
 (* [Local] functions here can only be called from the thread owning given queue
-  (at a given time). In the case of [steal], [from] parameter is unrestricted.    
+  (at a given time). In the case of [steal], [from] parameter is unrestricted.
 
-  This restriction improves performance as [push], [pop], [resize] need to 
-  linearize only with [steal]. 
+  This restriction improves performance as [push], [pop], [resize] need to
+  linearize only with [steal].
 *)
 module Local : sig
   val push : 'a t -> 'a -> bool
@@ -24,16 +18,16 @@ module Local : sig
 
   val is_empty : 'a t -> bool
   (** [is_empty] thoroughly checks if there's any elements in the array.
-    Call before [steal], which assumes there's room in the queue. *)  
+    Call before [steal], which assumes there's room in the queue. *)
 end
 
-module Resizable : sig 
-  type 'a t 
-  
+module Resizable : sig
+  type 'a t
+
   val create : size_exponent:int -> unit -> 'a t
 
   (* [Local] functions to be called only from the owner thread. *)
-  module Local : sig 
+  module Local : sig
     val push : 'a t -> 'a -> bool
     val pop : 'a t -> 'a option
     val steal : from:'a t -> 'a t -> int
@@ -41,19 +35,19 @@ module Resizable : sig
 
     val is_empty : 'a t -> bool
     (** [is_empty] thoroughly checks if there's any elements in the array.
-      Call before [steal], which assumes there's room in the queue. *)  
+      Call before [steal], which assumes there's room in the queue. *)
 
     val resize : 'a t -> unit
     val push_with_autoresize : 'a t -> 'a -> unit
   end
 end
 
-(** [indicative_size] gives some idea about the size of the queue. 
-    It's actual queue size when called from enqueuer thread (or while 
+(** [indicative_size] gives some idea about the size of the queue.
+    It's actual queue size when called from enqueuer thread (or while
     enqueuer is not inserting elements). *)
 val indicative_size : 'a t -> int
 
 (* [M] is the interface for Domainslib *)
-module M : sig 
+module M : sig
   include Ws_deque.S with type 'a t := 'a Resizable.t
 end
